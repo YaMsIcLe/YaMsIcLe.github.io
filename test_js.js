fd1d73db7482a64103136d0eadbeba06a2c77997
YaMsIcLe <cyamamotosan@gmail.com>
2019-03-04 17:34:05 -0800

    did my test/ example/ try js
    js stuffffssss

---
+++ test_js.js
@@ -0,0 +1,14 @@
+console.log("memesss");
+let a = window.prompt("Give a noun");
+let b = window.prompt("Give a second noun");
+let c = window.prompt("Give a verb");
+let d = window.prompt("Give a second verb");
+let e = window.prompt("Give an adjective");
+let f = window.prompt("Give a second adjective ");
+console.log (a , b , c , d , e , f)
+document.getElementById('nouna').innerText = a
+document.getElementById('nounb').innerText = b
+document.getElementById('verba').innerText = c
+document.getElementById('verbb').innerText = d
+document.getElementById('adja').innerText = e
+document.getElementById('adjb').innerText = f
