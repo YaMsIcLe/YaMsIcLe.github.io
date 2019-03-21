3c5835e23d440ef7973cb6e7deae8ca0e25b6cf2
MissionBit <missionbit@80p-MissionBit.local>
2019-03-20 17:51:02 -0700

    some flower\

--- test_js.js
+++ test_js.js
@@ -1,5 +1,21 @@
 console.log(' I have never wanted to die so much in my life');
 
 let draw = SVG('drawing').size(600, 600);
-let rectWidth = 50
-let rectHeight = 50
+let flowerRadius = 70
+let petalRadius = 60
+
+function drawflower {
+
+    
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+    let petal = draw.circle(petalRadius).fill('red').move(100, 100)
+
+    let flower = draw.circle(flowerRadius).fill('yellow').move(100, 100)
+
+}
+
