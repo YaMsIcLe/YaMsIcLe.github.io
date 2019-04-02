476b895d7f3700870788e0ea7a500f5b05bda08c
MissionBit <missionbit@80p-MissionBit.local>
2019-04-01 17:14:16 -0700

    memesss

--- test_js.js
+++ test_js.js
@@ -1,1 +1,31 @@
 console.log(' I have never wanted to die so much in my life');
+
+const canvasWidth = 700
+const canvasHeight = 500
+const blockWidth = 50
+const blockHeight = 25
+const rowNumb = 5
+const blockNumb = 10
+const PINK = '#FFC0CB'
+const BLACK = '#000'
+
+let draw = SVG('drawing').size(canvasWidth, canvasHeight);
+
+for (let i = 0; i < rowNumb; i ++){
+    let rowA = i * blockHeight;
+
+
+    for (let j = 0; j < blockNumb; j++){
+       let blockA = j * blockWidth;
+       
+       let rect = {
+           element: draw.rect(blockWidth, blockHeight),
+           color: PINK
+       };
+
+       rect.element.move(blockA, rowA);
+       rect.element.stroke(BLACK);
+       rect.element.fill(PINK);
+
+    }
+}
