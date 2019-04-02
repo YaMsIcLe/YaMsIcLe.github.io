e8c69c4e7640d72512adeb04ee544bf5a634e7e5
MissionBit <missionbit@80p-MissionBit.local>
2019-04-01 18:02:14 -0700

    memes

--- test_js.js
+++ test_js.js
@@ -1,32 +1,33 @@
 console.log(' I have never wanted to die so much in my life');
 
-const canvasWidth = 700
+const canvasWidth = 900
 const canvasHeight = 500
 const blockWidth = 50
 const blockHeight = 25
-const rowNumb = 5
+const rowNumb = 10
 const blockNumb = 10
 const PINK = '#FFC0CB'
 const BLACK = '#000'
 const xShift = 25
-let numbBlock = 1
+let numbBlock = 10
+// if row 10 has 10 rows, and row 9 has 9 blocks, how the HELL DO I DO IT //
 
 let draw = SVG('drawing').size(canvasWidth, canvasHeight);
 
 for (let i = 0; i < rowNumb; i ++){
     let rowA = i * blockHeight;
-    
+    let blockRowPer = i + 1
 
-    for (let j = 0; j < blockNumb; j++){
+    for (let j = 0; j < blockRowPer; j++){
        let blockA = j * blockWidth;
-
+       
+    
        let rect = {
-           element: draw.rect(blockWidth, blockHeight),
-           color: PINK
-           
-       };
+        element: draw.rect(blockWidth, blockHeight),
+        color: PINK
 
-       rect.element.move(blockA + rowA, rowA);
+       };
+       rect.element.move((canvasWidth/2) +(blockA - rowA),(canvasWidth/3.8 + rowA));
        rect.element.stroke(BLACK);
        rect.element.fill(PINK);
        
